7210f383afd5f9f46c3ac76b264aaf24fe52d5b8
JericFederer <pianocoderodyssey@gmail.com>
2024-06-27 17:28:56 +0900

    initial commit

---
+++ links.js
@@ -0,0 +1,38 @@
+export const links = [
+  {
+    name: "Restart iPhone App",
+    name2: "GitHub Repository",
+    link: "https://github.com/JericFederer97/swift-ui-restart-app",
+    image: "/git.svg",
+  },
+  {
+    name: "Pinch iPhone App",
+    name2: "GitHub Repository",
+    link: "https://github.com/JericFederer97/swift-ui-pinch-app",
+    image: "/git2.svg",
+  },
+  {
+    name: "Fructus iPhone App",
+    name2: "GitHub Repository",
+    link: "https://github.com/JericFederer97/swift-ui-fructus-app",
+    image: "/git.svg",
+  },
+  {
+    name: "Africa iPhone App",
+    name2: "GitHub Repository",
+    link: "https://github.com/JericFederer97/swift-ui-africa-app",
+    image: "/git2.svg",
+  },
+  {
+    name: "Touch Down iPhone App",
+    name2: "GitHub Repository",
+    link: "https://github.com/JericFederer97/swift-ui-touch-down-app",
+    image: "/git.svg",
+  },
+  {
+    name: "Devote iPhone App",
+    name2: "GitHub Repository",
+    link: "https://github.com/JericFederer97/swift-ui-devote-app",
+    image: "/git2.svg",
+  },
+];
---
+++ main.js
@@ -0,0 +1,39 @@
+import { links } from "./links.js";
+
+const linkContainer = document.getElementById("links");
+
+function addLink(name, name2, link, image) {
+  const textOne = ` 
+    <a href="${ link }" class="link" target="blank"> 
+      <img src="${ image }"/> 
+      <span>${ name }</span>
+      <img class="linkIcon" src="link-solid.svg" alt=""/> 
+    </a> 
+  `
+  const textTwo = ` 
+    <a href="${ link }" class="link" target="blank">
+      <div class=image>
+        <img src=${ image } alt='' />
+      </div>
+      <div class="names">
+        <span class="nameOne">${ name }</span>
+        <span class="nameTwo">${ name2 }</span>
+      </div> 
+    </a> 
+  `
+
+  return textTwo;
+}
+
+let allLinks = "";
+
+links.forEach((ele) => {
+    let link = ele.link;
+    let name = ele.name;
+    let name2 = ele.name2;
+    let image = ele.image;
+
+    allLinks += addLink(name, name2, link, image);
+});
+
+linkContainer.innerHTML = allLinks;
